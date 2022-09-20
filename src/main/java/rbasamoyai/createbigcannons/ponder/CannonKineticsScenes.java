9278f624d8dcf7031269be2baf3506d3836bd5e6
rbasamoyai <b33gy0sh@gmail.com>
2022-09-20 13:12:39 -0400

    Started work on cannon kinetics ponders

---
+++ src/main/java/rbasamoyai/createbigcannons/ponder/CannonKineticsScenes.java
@@ -0,0 +1,12 @@
+package rbasamoyai.createbigcannons.ponder;
+
+import com.simibubi.create.foundation.ponder.SceneBuilder;
+import com.simibubi.create.foundation.ponder.SceneBuildingUtil;
+
+public class CannonKineticsScenes {
+
+	public static void slidingBreech(SceneBuilder scene, SceneBuildingUtil util) {
+		
+	}
+	
+}
