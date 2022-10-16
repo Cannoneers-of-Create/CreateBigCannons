8e3918a2fbb7c76ab0a3cec2aa41e9d938c9d78d
rbasamoyai <b33gy0sh@gmail.com>
2022-10-16 16:49:55 -0400

    Added custom fluid fog colors to molten metal fluids

--- src/main/java/rbasamoyai/createbigcannons/CreateBigCannonsClient.java
+++ src/main/java/rbasamoyai/createbigcannons/CreateBigCannonsClient.java
@@ -1,8 +1,18 @@
 
 package rbasamoyai.createbigcannons;
 
+import java.util.Arrays;
+import java.util.List;
+
+import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.ParticleEngine;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraftforge.client.event.EntityViewRenderEvent.FogColors;
+import net.minecraftforge.client.event.EntityViewRenderEvent.RenderFogEvent;
 import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -15,6 +25,9 @@ public class CreateBigCannonsClient {
 		CBCBlockPartials.init();
 		modEventBus.addListener(CreateBigCannonsClient::onClientSetup);
 		modEventBus.addListener(CreateBigCannonsClient::onRegisterParticleFactories);
+		
+		forgeEventBus.addListener(CreateBigCannonsClient::getFogColor);
+		forgeEventBus.addListener(CreateBigCannonsClient::getFogDensity);
 	}
 	
 	public static void onRegisterParticleFactories(ParticleFactoryRegisterEvent event) {
@@ -28,6 +41,69 @@ public class CreateBigCannonsClient {
 		CBCPonderIndex.register();
 		CBCPonderIndex.registerTags();
 		CBCBlockPartials.resolveDeferredModels();
+	}
+	
+	public static void getFogColor(FogColors event) {
+		Camera info = event.getCamera();
+		Minecraft mc = Minecraft.getInstance();
+		Level level = mc.level;
+		BlockPos blockPos = info.getBlockPosition();
+		FluidState fluidState = level.getFluidState(blockPos);
+		if (info.getPosition().y > blockPos.getY() + fluidState.getHeight(level, blockPos)) return;
+
+		Fluid fluid = fluidState.getType();
+
+		if (CBCFluids.MOLTEN_CAST_IRON.get().isSame(fluid)) {
+			event.setRed(70 / 255f);
+			event.setGreen(10 / 255f);
+			event.setBlue(11 / 255f);
+			return;
+		}
+		if (CBCFluids.MOLTEN_BRONZE.get().isSame(fluid)) {
+			event.setRed(99 / 255f);
+			event.setGreen(66 / 255f);
+			event.setBlue(22 / 255f);
+			return;
+		}
+		if (CBCFluids.MOLTEN_STEEL.get().isSame(fluid)) {
+			event.setRed(111 / 255f);
+			event.setGreen(110 / 255f);
+			event.setBlue(106 / 255f);
+			return;
+		}
+		if (CBCFluids.MOLTEN_NETHERSTEEL.get().isSame(fluid)) {
+			event.setRed(76 / 255f);
+			event.setGreen(50 / 255f);
+			event.setBlue(58 / 255f);
+			return;
+		}
+	}
+	
+	public static void getFogDensity(RenderFogEvent event) {
+		if (!event.isCancelable()) return;
+		
+		Camera info = event.getCamera();
+		Minecraft mc = Minecraft.getInstance();
+		Level level = mc.level;
+		BlockPos blockPos = info.getBlockPosition();
+		FluidState fluidState = level.getFluidState(blockPos);
+		if (info.getPosition().y > blockPos.getY() + fluidState.getHeight(level, blockPos)) return;
+
+		Fluid fluid = fluidState.getType();
+		
+		List<Fluid> moltenMetals = Arrays.asList(
+				CBCFluids.MOLTEN_CAST_IRON.get(),
+				CBCFluids.MOLTEN_BRONZE.get(),
+				CBCFluids.MOLTEN_STEEL.get(),
+				CBCFluids.MOLTEN_NETHERSTEEL.get());
+		
+		for (Fluid fluid1 : moltenMetals) {
+			if (fluid1.isSame(fluid)) {
+				event.scaleFarPlaneDistance(1f / 32f);
+				event.setCanceled(true);
+				return;
+			}
+		}
 	}
 	
 }
