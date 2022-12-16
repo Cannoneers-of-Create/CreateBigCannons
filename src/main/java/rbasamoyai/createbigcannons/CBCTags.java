a2a7e3352048cd97a3302e48a2b14dba267f5da9
rbasamoyai <b33gy0sh@gmail.com>
2022-12-15 22:47:48 -0500

    Overhauled impact system for different block breaking categories

--- src/main/java/rbasamoyai/createbigcannons/CBCTags.java
+++ src/main/java/rbasamoyai/createbigcannons/CBCTags.java
@@ -1,14 +1,8 @@
 package rbasamoyai.createbigcannons;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Supplier;
-
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllTags;
 import com.tterrag.registrate.providers.ProviderType;
-
 import net.minecraft.data.tags.TagsProvider.TagAppender;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
@@ -21,6 +15,11 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
 
 import static rbasamoyai.createbigcannons.CreateBigCannons.REGISTRATE;
 
@@ -35,7 +34,16 @@ public class CBCTags {
 			GRAPESHOT_SHATTERABLE = createAndGenerateBlockTag(CreateBigCannons.resource("grapeshot_shatterable")),
 			GRAPESHOT_VULNERABLE = createAndGenerateBlockTag(CreateBigCannons.resource("grapeshot_vulnerable")),
 			WEAK_CANNON_END = createAndGenerateBlockTag(CreateBigCannons.resource("weak_cannon_end")),
-			DRILL_CAN_PASS_THROUGH = createAndGenerateBlockTag(CreateBigCannons.resource("drill_can_pass_through"));
+			DRILL_CAN_PASS_THROUGH = createAndGenerateBlockTag(CreateBigCannons.resource("drill_can_pass_through")),
+			BIG_CANNON_NO_RESISTANCE = createAndGenerateBlockTag(CreateBigCannons.resource("big_cannon_no_resistance")),
+			BIG_CANNON_RESISTANT = createAndGenerateBlockTag(CreateBigCannons.resource("big_cannon_resistant")),
+			BIG_CANNON_BLOCKING = createAndGenerateBlockTag(CreateBigCannons.resource("big_cannon_absorbing")),
+			MEDIUM_CANNON_NO_RESISTANCE = createAndGenerateBlockTag(CreateBigCannons.resource("medium_cannon_no_resistance")),
+			MEDIUM_CANNON_RESISTANT = createAndGenerateBlockTag(CreateBigCannons.resource("medium_cannon_resistant")),
+			MEDIUM_CANNON_BLOCKING = createAndGenerateBlockTag(CreateBigCannons.resource("medium_cannon_blocking")),
+			AUTOCANNON_NO_RESISTANCE = createAndGenerateBlockTag(CreateBigCannons.resource("autocannon_no_resistance")),
+			AUTOCANNON_RESISTANT = createAndGenerateBlockTag(CreateBigCannons.resource("autocannon_resistant")),
+			AUTOCANNON_BLOCKING = createAndGenerateBlockTag(CreateBigCannons.resource("autocannon_resistant"));
 		
 		public static TagKey<Block> createAndGenerateBlockTag(ResourceLocation loc) {
 			TagKey<Block> tag = BlockTags.create(loc); 
@@ -84,6 +92,13 @@ public class CBCTags {
 			addBlocksToBlockTag(GRAPESHOT_VULNERABLE, () -> Arrays.asList(AllBlocks.BRASS_CASING.get(), AllBlocks.BRASS_ENCASED_COGWHEEL.get(), AllBlocks.BRASS_ENCASED_LARGE_COGWHEEL.get(), AllBlocks.BRASS_ENCASED_SHAFT.get(), AllBlocks.MECHANICAL_CRAFTER.get(), AllBlocks.MECHANICAL_ARM.get()));
 			addBlocksToBlockTag(GRAPESHOT_VULNERABLE, () -> Arrays.asList(AllBlocks.COPPER_CASING.get(), AllBlocks.ENCASED_FLUID_PIPE.get(), AllBlocks.FLUID_PIPE.get(), AllBlocks.GLASS_FLUID_PIPE.get(), AllBlocks.FLUID_VALVE.get(), AllBlocks.COPPER_VALVE_HANDLE.get(), AllBlocks.MECHANICAL_PUMP.get(), AllBlocks.HOSE_PULLEY.get()));
 			addBlocksToBlockTag(GRAPESHOT_VULNERABLE, () -> Arrays.asList(AllBlocks.BRASS_TUNNEL.get(), AllBlocks.BRASS_BELT_FUNNEL.get(), AllBlocks.BRASS_FUNNEL.get()));
+
+			addTagsToBlockTag(BIG_CANNON_RESISTANT, () -> Arrays.asList(Tags.Blocks.OBSIDIAN));
+
+			addTagsToBlockTag(MEDIUM_CANNON_RESISTANT, () -> Arrays.asList(Tags.Blocks.STORAGE_BLOCKS_IRON, Tags.Blocks.STORAGE_BLOCKS_DIAMOND));
+			addTagsToBlockTag(MEDIUM_CANNON_BLOCKING, () -> Arrays.asList(Tags.Blocks.OBSIDIAN));
+
+			addTagsToBlockTag(AUTOCANNON_BLOCKING, () -> Arrays.asList(Tags.Blocks.OBSIDIAN));
 		}
 	}
 	
--- src/main/java/rbasamoyai/createbigcannons/munitions/autocannon/AbstractAutocannonProjectile.java
+++ src/main/java/rbasamoyai/createbigcannons/munitions/autocannon/AbstractAutocannonProjectile.java
@@ -3,73 +3,88 @@ package rbasamoyai.createbigcannons.munitions.autocannon;
 import com.simibubi.create.content.contraptions.particle.AirParticleData;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.BlockHitResult;
+import rbasamoyai.createbigcannons.CBCTags;
 import rbasamoyai.createbigcannons.munitions.AbstractCannonProjectile;
 
 public abstract class AbstractAutocannonProjectile extends AbstractCannonProjectile {
 
-    protected int ageRemaining;
+	protected int ageRemaining;
 
-    protected AbstractAutocannonProjectile(EntityType<? extends AbstractAutocannonProjectile> type, Level level) {
-        super(type, level);
-        this.ageRemaining = 60;
-        this.damage = 15;
-    }
+	protected AbstractAutocannonProjectile(EntityType<? extends AbstractAutocannonProjectile> type, Level level) {
+		super(type, level);
+		this.ageRemaining = 60;
+		this.damage = 15;
+	}
 
-    @Override protected float getKnockback(Entity target) { return 1.0f; }
+	@Override protected float getKnockback(Entity target) { return 0.5f; }
 
-    @Override
-    public void tick() {
-        super.tick();
+	@Override
+	public void tick() {
+		super.tick();
 
-        if (!this.level.isClientSide) {
-            this.ageRemaining--;
-            if (this.ageRemaining <= 0) this.discard();
-        }
-    }
+		if (!this.level.isClientSide) {
+			this.ageRemaining--;
+			if (this.ageRemaining <= 0) this.discard();
+		}
+	}
 
-    public void setTracer(boolean tracer) {
-        if (tracer) {
-            this.entityData.set(ID_FLAGS, (byte)(this.entityData.get(ID_FLAGS) | 2));
-        } else {
-            this.entityData.set(ID_FLAGS, (byte)(this.entityData.get(ID_FLAGS) & 0b11111101));
-        }
-    }
+	public void setTracer(boolean tracer) {
+		if (tracer) {
+			this.entityData.set(ID_FLAGS, (byte)(this.entityData.get(ID_FLAGS) | 2));
+		} else {
+			this.entityData.set(ID_FLAGS, (byte)(this.entityData.get(ID_FLAGS) & 0b11111101));
+		}
+	}
 
-    public boolean isTracer() { return (this.entityData.get(ID_FLAGS) & 2) != 0; }
+	public boolean isTracer() { return (this.entityData.get(ID_FLAGS) & 2) != 0; }
 
-    @Override public final BlockState getRenderedBlockState() { return Blocks.AIR.defaultBlockState(); }
+	@Override public final BlockState getRenderedBlockState() { return Blocks.AIR.defaultBlockState(); }
 
-    @Override protected ParticleOptions getTrailParticle() { return new AirParticleData(1, 10); }
+	@Override protected ParticleOptions getTrailParticle() { return new AirParticleData(1, 10); }
 
-    @Override protected float getGravity() { return 0; }
-    @Override protected float getInertia() { return 1; }
+	@Override protected float getGravity() { return 0; }
+	@Override protected float getInertia() { return 1; }
 
-    @Override
-    public void addAdditionalSaveData(CompoundTag tag) {
-        super.addAdditionalSaveData(tag);
-        tag.putBoolean("Tracer", this.isTracer());
-        tag.putInt("Age", this.ageRemaining);
-    }
+    @Override protected TagKey<Block> blockingTag() { return CBCTags.BlockCBC.AUTOCANNON_BLOCKING; }
+    @Override protected TagKey<Block> resistantTag() { return CBCTags.BlockCBC.AUTOCANNON_RESISTANT; }
+    @Override protected TagKey<Block> noResistanceTag() { return CBCTags.BlockCBC.AUTOCANNON_NO_RESISTANCE; }
 
     @Override
-    public void readAdditionalSaveData(CompoundTag tag) {
-        super.readAdditionalSaveData(tag);
-        this.setTracer(tag.getBoolean("Tracer"));
-        this.ageRemaining = tag.getInt("Age");
+    protected boolean isResistantFallback(BlockHitResult result, BlockState hitBlock) {
+        Material material = hitBlock.getMaterial();
+        return material == Material.STONE || material == Material.METAL || material == Material.HEAVY_METAL;
     }
 
-    public static void buildAutocannon(EntityType.Builder<? extends AbstractAutocannonProjectile> builder) {
-        builder.setTrackingRange(16)
-                .setUpdateInterval(1)
-                .setShouldReceiveVelocityUpdates(true)
-                .fireImmune()
-                .sized(0.2f, 0.2f);
-    }
+    @Override
+	public void addAdditionalSaveData(CompoundTag tag) {
+		super.addAdditionalSaveData(tag);
+		tag.putBoolean("Tracer", this.isTracer());
+		tag.putInt("Age", this.ageRemaining);
+	}
 
-    @Override protected float getPenetratingExplosionPower() { return 1; }
+	@Override
+	public void readAdditionalSaveData(CompoundTag tag) {
+		super.readAdditionalSaveData(tag);
+		this.setTracer(tag.getBoolean("Tracer"));
+		this.ageRemaining = tag.getInt("Age");
+	}
+
+	public static void buildAutocannon(EntityType.Builder<? extends AbstractAutocannonProjectile> builder) {
+		builder.setTrackingRange(16)
+				.setUpdateInterval(1)
+				.setShouldReceiveVelocityUpdates(true)
+				.fireImmune()
+				.sized(0.2f, 0.2f);
+	}
+
+	@Override protected float getPenetratingExplosionPower() { return 1; }
 }
