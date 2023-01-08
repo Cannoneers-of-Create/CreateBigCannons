a8409dcbca900b859740f9935b460fb82f717dc7
TropheusJ <jverrellijr@icloud.com>
2023-01-08 05:05:24 -0500

    improve stuff still haven't figured out create fabric remapping issues - add platform to version - automatically fill in dependencies in fmj and mods.toml - stop warning about no minVersion on forge - gradle cleanup - default build workflow - test for porting lib access

--- fabric/src/main/java/net/examplemod/fabric/ExampleModFabric.java
+++ fabric/src/main/java/net/examplemod/fabric/ExampleModFabric.java
@@ -1,5 +1,6 @@
 package net.examplemod.fabric;
 
+import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
 import net.examplemod.ExampleMod;
 import net.fabricmc.api.ModInitializer;
 
@@ -7,5 +8,9 @@ public class ExampleModFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         ExampleMod.init();
+        ExampleMod.LOGGER.info(EnvExecutor.unsafeRunForDist(
+                () -> () -> "{} is accessing Porting Lib on a fabric client!",
+                () -> () -> "{} is accessing Porting Lib on a fabric server!"
+                ), ExampleMod.NAME);
     }
 }
