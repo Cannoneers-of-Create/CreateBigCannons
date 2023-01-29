3a2b9728bf3039cf10d369c205cf6bda756b71e7
rbasamoyai <b33gy0sh@gmail.com>
2023-01-28 20:24:01 -0500

    🐛 Fix clothing on handle autocannon

--- src/main/java/rbasamoyai/createbigcannons/mixin/HumanoidPoseMixin.java
+++ src/main/java/rbasamoyai/createbigcannons/mixin/HumanoidPoseMixin.java
@@ -2,9 +2,12 @@ package rbasamoyai.createbigcannons.mixin;
 
 import net.minecraft.client.model.AgeableListModel;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,18 +17,22 @@ import rbasamoyai.createbigcannons.cannon_control.carriage.CannonCarriageEntity;
 @Mixin(HumanoidModel.class)
 public abstract class HumanoidPoseMixin extends AgeableListModel {
 
-    @Inject(method = "Lnet/minecraft/client/model/HumanoidModel;setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At("HEAD"))
+    @Shadow @Final public ModelPart hat;
+
+    @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At("HEAD"))
     public <T extends LivingEntity> void createbigcannons$setupAnimHead(T livingEntity, float f, float g, float h, float i, float j, CallbackInfo ci) {
         Entity vehicle = livingEntity.getVehicle();
         if (vehicle instanceof CannonCarriageEntity carriage && !carriage.isCannonRider())
             this.riding = false;
     }
 
-    @Inject(method = "Lnet/minecraft/client/model/HumanoidModel;setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At("TAIL"))
-    public <T extends LivingEntity> void createbigcannons$setupAnimTail(T livingEntity, float f, float g, float h, float i, float j, CallbackInfo ci) {
+    @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At("TAIL"))
+    public <T extends LivingEntity> void createbigcannons$setupAnimNearTail(T livingEntity, float f, float g, float h, float i, float j, CallbackInfo ci) {
         HumanoidModel<?> self = (HumanoidModel<?>) (Object) this;
         if (CBCEntityTypes.PITCH_ORIENTED_CONTRAPTION.is(livingEntity.getVehicle())) {
             self.head.xRot = 0;
+            self.hat.copyFrom(self.head);
+
             self.leftArm.xRot = -1.25f;
             self.leftArm.yRot = 0.25f;
             self.leftArm.zRot = 0;
