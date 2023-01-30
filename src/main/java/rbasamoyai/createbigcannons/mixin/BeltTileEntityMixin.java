63898830f0e48144efd9926b79fa8c262c912846
rbasamoyai <b33gy0sh@gmail.com>
2023-01-29 19:53:39 -0500

    🐛 Fix cannon contraptions being moved by conveyor belts

---
+++ src/main/java/rbasamoyai/createbigcannons/mixin/BeltTileEntityMixin.java
@@ -0,0 +1,19 @@
+package rbasamoyai.createbigcannons.mixin;
+
+import com.simibubi.create.content.contraptions.relays.belt.transport.BeltMovementHandler;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import rbasamoyai.createbigcannons.cannon_control.contraption.PitchOrientedContraptionEntity;
+
+@Mixin(BeltMovementHandler.class)
+public class BeltTileEntityMixin {
+
+	@Inject(at = @At("HEAD"), method = "canBeTransported", remap = false, cancellable = true)
+	private static void createbigcannons$canBeTransported(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+		if (entity instanceof PitchOrientedContraptionEntity) cir.setReturnValue(false);
+	}
+
+}
