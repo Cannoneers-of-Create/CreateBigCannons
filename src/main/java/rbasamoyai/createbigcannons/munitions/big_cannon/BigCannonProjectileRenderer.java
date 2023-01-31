7b8d842842c6116235ba22d10922347f562b2bf8
rbasamoyai <b33gy0sh@gmail.com>
2023-01-30 20:37:49 -0500

    🐛 Fix BigCannonProjectileRenderer

--- src/main/java/rbasamoyai/createbigcannons/munitions/big_cannon/BigCannonProjectileRenderer.java
+++ src/main/java/rbasamoyai/createbigcannons/munitions/big_cannon/BigCannonProjectileRenderer.java
@@ -1,16 +1,16 @@
 package rbasamoyai.createbigcannons.munitions.big_cannon;
 
-import com.jozufozu.flywheel.core.virtual.VirtualEmptyModelData;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Quaternion;
 import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -27,14 +27,15 @@ public class BigCannonProjectileRenderer<T extends AbstractBigCannonProjectile> 
 			poseStack.pushPose();
 			
 			Quaternion q = Vector3f.YP.rotationDegrees(entity.getViewYRot(partialTicks) + 180.0f);
-			Quaternion q1 = Vector3f.XP.rotationDegrees(entity.getViewXRot(partialTicks));
+			Quaternion q1 = Vector3f.XP.rotationDegrees(entity.getViewXRot(partialTicks) - 90.0f);
 			q.mul(q1);
+
+			poseStack.translate(0.0d, 0.4d, 0.0d);
 			poseStack.mulPose(q);
-			
-			poseStack.translate(-0.5d, -0.1d, -0.5d);
-			
-			BlockRenderDispatcher brd = Minecraft.getInstance().getBlockRenderer();
-			brd.renderSingleBlock(blockState, poseStack, buffers, packedLight, OverlayTexture.NO_OVERLAY, VirtualEmptyModelData.INSTANCE);
+
+			Minecraft.getInstance().getItemRenderer()
+					.renderStatic(new ItemStack(blockState.getBlock()), ItemTransforms.TransformType.NONE, packedLight,
+							OverlayTexture.NO_OVERLAY, poseStack, buffers, 0);
 			
 			poseStack.popPose();
 			super.render(entity, entityYaw, partialTicks, poseStack, buffers, packedLight);
