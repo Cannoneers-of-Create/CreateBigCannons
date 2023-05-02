695444954fc4ff22c1f6411aeac072a276b6e2d0
Max <sebastian.8295.8295@gmail.com>
2023-05-02 19:58:00 +0100

    🌌 added resourceLocation to the event

--- common/src/main/java/rbasamoyai/createbigcannons/multiloader/event_classes/OnCannonBreakBlock.java
+++ common/src/main/java/rbasamoyai/createbigcannons/multiloader/event_classes/OnCannonBreakBlock.java
@@ -1,9 +1,11 @@
 package rbasamoyai.createbigcannons.multiloader.event_classes;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.state.BlockState;
 
 public interface OnCannonBreakBlock {
 	BlockPos getAffectedBlockPos();
 	BlockState getAffectedBlockState();
+	ResourceLocation getResourceLocation();
 }
--- fabric/src/main/java/rbasamoyai/createbigcannons/fabric/events/OnCannonBreakBlockImpl.java
+++ fabric/src/main/java/rbasamoyai/createbigcannons/fabric/events/OnCannonBreakBlockImpl.java
@@ -1,15 +1,18 @@
 package rbasamoyai.createbigcannons.fabric.events;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.state.BlockState;
 import rbasamoyai.createbigcannons.multiloader.event_classes.OnCannonBreakBlock;
 
 public class OnCannonBreakBlockImpl implements OnCannonBreakBlock {
 	private BlockPos blockPos;
 	private BlockState blockState;
-	public OnCannonBreakBlockImpl(BlockPos blockPos, BlockState blockState) {
+	private ResourceLocation resourceLocation;
+	public OnCannonBreakBlockImpl(BlockPos blockPos, BlockState blockState, ResourceLocation resourceLocation) {
 		this.blockPos = blockPos;
 		this.blockState = blockState;
+		this.resourceLocation = resourceLocation;
 	}
 	@Override
 	public BlockPos getAffectedBlockPos() {
@@ -19,5 +22,9 @@ public class OnCannonBreakBlockImpl implements OnCannonBreakBlock {
 	@Override
 	public BlockState getAffectedBlockState() {
 		return this.blockState;
+	}
+	@Override
+	public ResourceLocation getResourceLocation() {
+		return this.resourceLocation;
 	}
 }
