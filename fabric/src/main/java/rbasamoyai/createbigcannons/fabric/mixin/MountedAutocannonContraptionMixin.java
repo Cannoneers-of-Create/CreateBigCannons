a89d2bcb8d4e2cecd634d5bd51f94492c3b74026
Max <sebastian.8295.8295@gmail.com>
2023-06-01 11:55:49 +0100

    Fixed subprojects for fabric and forge

--- fabric/src/main/java/rbasamoyai/createbigcannons/fabric/mixin/MountedAutocannonContraptionMixin.java
+++ fabric/src/main/java/rbasamoyai/createbigcannons/fabric/mixin/MountedAutocannonContraptionMixin.java
@@ -1,18 +1,19 @@
 package rbasamoyai.createbigcannons.fabric.mixin;
 
+import javax.annotation.Nullable;
+
+import org.spongepowered.asm.mixin.Mixin;
+
 import io.github.fabricators_of_create.porting_lib.transfer.TransferUtil;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
 import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.world.item.ItemStack;
-import org.spongepowered.asm.mixin.Mixin;
 import rbasamoyai.createbigcannons.cannon_control.contraption.AbstractMountedCannonContraption;
 import rbasamoyai.createbigcannons.cannon_control.contraption.ItemCannon;
 import rbasamoyai.createbigcannons.cannon_control.contraption.MountedAutocannonContraption;
 import rbasamoyai.createbigcannons.fabric.cannons.AutocannonBreechBlockEntity;
 import rbasamoyai.createbigcannons.fabric.mixin_interface.GetItemStorage;
-
-import javax.annotation.Nullable;
 
 @Mixin(MountedAutocannonContraption.class)
 public abstract class MountedAutocannonContraptionMixin extends AbstractMountedCannonContraption implements ItemCannon, GetItemStorage {
@@ -56,7 +57,7 @@ public abstract class MountedAutocannonContraptionMixin extends AbstractMountedC
 	@Nullable
 	@Override
 	public Storage<ItemVariant> getItemStorage() {
-		return this.presentTileEntities.get(this.startPos) instanceof AutocannonBreechBlockEntity breech ? breech.createItemHandler() : null;
+		return this.presentBlockEntities.get(this.startPos) instanceof AutocannonBreechBlockEntity breech ? breech.createItemHandler() : null;
 	}
 
 }
--- forge/src/main/java/rbasamoyai/createbigcannons/forge/mixin/MountedAutocannonContraptionMixin.java
+++ forge/src/main/java/rbasamoyai/createbigcannons/forge/mixin/MountedAutocannonContraptionMixin.java
@@ -1,16 +1,17 @@
 package rbasamoyai.createbigcannons.forge.mixin;
 
+import javax.annotation.Nonnull;
+
+import org.spongepowered.asm.mixin.Mixin;
+
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.IItemHandler;
-import org.spongepowered.asm.mixin.Mixin;
 import rbasamoyai.createbigcannons.cannon_control.contraption.AbstractMountedCannonContraption;
 import rbasamoyai.createbigcannons.cannon_control.contraption.ItemCannon;
 import rbasamoyai.createbigcannons.cannon_control.contraption.MountedAutocannonContraption;
 import rbasamoyai.createbigcannons.forge.cannons.AutocannonBreechBlockEntity;
 import rbasamoyai.createbigcannons.forge.mixin_interface.GetItemStorage;
-
-import javax.annotation.Nonnull;
 
 @Mixin(MountedAutocannonContraption.class)
 public abstract class MountedAutocannonContraptionMixin extends AbstractMountedCannonContraption implements ItemCannon, GetItemStorage {
@@ -28,9 +29,9 @@ public abstract class MountedAutocannonContraptionMixin extends AbstractMountedC
 	@Nonnull
 	@Override
 	public LazyOptional<IItemHandler> getItemStorage() {
-		return this.presentTileEntities.get(this.startPos) instanceof AutocannonBreechBlockEntity breech
-				? LazyOptional.of(breech::createItemHandler)
-				: LazyOptional.empty();
+		return this.presentBlockEntities.get(this.startPos) instanceof AutocannonBreechBlockEntity breech
+			? LazyOptional.of(breech::createItemHandler)
+			: LazyOptional.empty();
 	}
 
 }
