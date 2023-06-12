7009e2a9dafdee35d152a73ebd5c30c0e263c3e5
Max <sebastian.8295.8295@gmail.com>
2023-06-12 05:47:53 +0100

    Fixed the fluids 😢

--- forge/src/main/java/rbasamoyai/createbigcannons/forge/mixin/CBCFlowingFluidMixin.java
+++ forge/src/main/java/rbasamoyai/createbigcannons/forge/mixin/CBCFlowingFluidMixin.java
@@ -1,17 +1,23 @@
 package rbasamoyai.createbigcannons.forge.mixin;
 
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+
+import com.simibubi.create.foundation.data.CreateRegistrate;
+
 import dev.architectury.patchedmixin.staticmixin.spongepowered.asm.mixin.Shadow;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.level.material.FlowingFluid;
 import net.minecraftforge.common.SoundActions;
 import net.minecraftforge.fluids.FluidType;
-import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
 import rbasamoyai.createbigcannons.index.fluid_utils.CBCFlowingFluid;
 
 @Mixin(CBCFlowingFluid.class)
 public abstract class CBCFlowingFluidMixin extends FlowingFluid {
+	@Shadow @Final protected ResourceLocation stillTex;
+	@Shadow @Final protected ResourceLocation flowingTex;
+	@Shadow @Final protected int color;
 	@Shadow
 	@Final
 	protected SoundEvent fillSound;
@@ -21,6 +27,6 @@ public abstract class CBCFlowingFluidMixin extends FlowingFluid {
 
 	@Override
 	public FluidType getFluidType() {
-		return new FluidType(FluidType.Properties.create().sound(SoundActions.BUCKET_FILL, this.fillSound).sound(SoundActions.BUCKET_EMPTY, this.emptySound));
+		return CreateRegistrate.defaultFluidType(FluidType.Properties.create().sound(SoundActions.BUCKET_FILL, this.fillSound).sound(SoundActions.BUCKET_EMPTY, this.emptySound), stillTex, flowingTex);
 	}
 }
