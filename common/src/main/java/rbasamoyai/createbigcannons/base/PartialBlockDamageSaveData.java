757c90b9572dfee67fc0b742b830e0ad306e40f6
rbasamoyai <b33gy0sh@gmail.com>
2023-06-29 11:15:07 -0400

    🐛 Fix dimensional partial damage

--- common/src/main/java/rbasamoyai/createbigcannons/base/PartialBlockDamageSaveData.java
+++ common/src/main/java/rbasamoyai/createbigcannons/base/PartialBlockDamageSaveData.java
@@ -2,11 +2,15 @@ package rbasamoyai.createbigcannons.base;
 
 import com.simibubi.create.foundation.utility.NBTHelper;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.saveddata.SavedData;
 
 import java.util.HashMap;
@@ -14,30 +18,44 @@ import java.util.Map;
 
 public class PartialBlockDamageSaveData extends SavedData {
 
-	private Map<BlockPos, Integer> blockDamage = new HashMap<>();
+	private static final int MAX_DAMAGES_PER_DIMENSION = 32768;
+
+	private Map<ResourceKey<Level>, Map<BlockPos, Integer>> blockDamage = new HashMap<>();
 
 	private PartialBlockDamageSaveData() {}
 
 	@Override
 	public CompoundTag save(CompoundTag tag) {
-		tag.put("BlockDamage", NBTHelper.writeCompoundList(this.blockDamage.entrySet(), e -> {
-			CompoundTag tag1 = new CompoundTag();
-			tag1.put("Pos", NbtUtils.writeBlockPos(e.getKey()));
-			tag1.putInt("Damage", e.getValue());
-			return tag1;
-		}));
+		CompoundTag blockDamage = new CompoundTag();
+		for (Map.Entry<ResourceKey<Level>, Map<BlockPos, Integer>> entry : this.blockDamage.entrySet()) {
+			blockDamage.put(entry.getKey().location().toString(), NBTHelper.writeCompoundList(entry.getValue().entrySet(), e -> {
+				CompoundTag tag1 = new CompoundTag();
+				tag1.put("Pos", NbtUtils.writeBlockPos(e.getKey()));
+				tag1.putInt("Damage", e.getValue());
+				return tag1;
+			}));
+		}
+		tag.put("BlockDamage", blockDamage);
 		return tag;
 	}
 
-	public Map<BlockPos, Integer> getBlockDamage() { return this.blockDamage; }
+	public Map<ResourceKey<Level>, Map<BlockPos, Integer>> getBlockDamage() { return this.blockDamage; }
 
 	private static PartialBlockDamageSaveData load(CompoundTag tag) {
 		PartialBlockDamageSaveData savedata = new PartialBlockDamageSaveData();
-		ListTag values = tag.getList("BlockDamage", Tag.TAG_COMPOUND);
-		int len = Math.min(values.size(), 32768);
-		for (int i = 0; i < len; ++i) {
-			CompoundTag entry = values.getCompound(i);
-			savedata.blockDamage.put(NbtUtils.readBlockPos(entry.getCompound("Pos")), entry.getInt("Damage"));
+		CompoundTag values = tag.getCompound("BlockDamage");
+
+		for (String key : values.getAllKeys()) {
+			ResourceKey<Level> dimensionKey = ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(key));
+			ListTag damagesTag = values.getList(key, Tag.TAG_COMPOUND);
+			Map<BlockPos, Integer> damages = new HashMap<>(damagesTag.size());
+
+			int len = Math.min(damagesTag.size(), MAX_DAMAGES_PER_DIMENSION);
+			for (int i = 0; i < len; ++i) {
+				CompoundTag entry = damagesTag.getCompound(i);
+				damages.put(NbtUtils.readBlockPos(entry.getCompound("Pos")), entry.getInt("Damage"));
+			}
+			savedata.blockDamage.put(dimensionKey, damages);
 		}
 		return savedata;
 	}
