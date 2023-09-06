755391cf1fd8cd96f5a5815be67a092cf1776e4d
rbasamoyai <b33gy0sh@gmail.com>
2023-09-06 17:33:27 -0400

    ⚡ Even more hooks

--- common/src/main/java/rbasamoyai/createbigcannons/CBCClientCommon.java
+++ common/src/main/java/rbasamoyai/createbigcannons/CBCClientCommon.java
@@ -180,16 +180,18 @@ public class CBCClientCommon {
 
 	public static boolean onPlayerRenderPre(PoseStack stack, AbstractClientPlayer player, PlayerRenderer renderer, float partialTicks) {
 		if (player.getVehicle() instanceof PitchOrientedContraptionEntity poce && poce.getSeatPos(player) != null) {
-			float yaw = (-Mth.lerp(partialTicks, player.yRotO, player.getYRot()) + 90) * Mth.DEG_TO_RAD;
-			yaw = modifyPlayerRenderYaw(yaw, partialTicks, player, poce);
+			float yaw = 90 - Mth.lerp(partialTicks, player.yRotO, player.getYRot());
 			float pitch = Mth.lerp(partialTicks, player.xRotO, player.getXRot());
 			pitch = modifyPlayerRenderPitch(pitch, partialTicks, player, poce);
 			float roll = 0;
 			roll = modifyPlayerRenderRoll(roll, partialTicks, player, poce);
+			float yaw1 = 0;
+			yaw1 = modifyPlayerRenderYaw(yaw1, partialTicks, player, poce);
 
-			Vector3f pitchVec = new Vector3f(Mth.sin(yaw), 0, Mth.cos(yaw));
+			Vector3f pitchVec = new Vector3f(Mth.sin(yaw * Mth.DEG_TO_RAD), 0, Mth.cos(yaw * Mth.DEG_TO_RAD));
 			Vector3f rollVec = new Vector3f(Mth.sin(yaw + Mth.HALF_PI), 0, Mth.cos(yaw + Mth.HALF_PI));
 			stack.mulPose(pitchVec.rotationDegrees(pitch));
+			stack.mulPose(Vector3f.YP.rotation(yaw1));
 			stack.mulPose(rollVec.rotationDegrees(roll));
 			stack.translate(0, -1.25, 0);
 		}
