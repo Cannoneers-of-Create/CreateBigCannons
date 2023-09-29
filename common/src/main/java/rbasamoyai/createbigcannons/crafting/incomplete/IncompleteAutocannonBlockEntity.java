0ab6ceb10a020e474a765d5d7afec33a033b01ea
rbasamoyai <b33gy0sh@gmail.com>
2023-09-28 20:22:57 -0400

    ✨🐛 Added autocannon boring to cannon crafting wand

--- common/src/main/java/rbasamoyai/createbigcannons/crafting/incomplete/IncompleteAutocannonBlockEntity.java
+++ common/src/main/java/rbasamoyai/createbigcannons/crafting/incomplete/IncompleteAutocannonBlockEntity.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.simibubi.create.content.equipment.goggles.IHaveHoveringInformation;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
@@ -12,10 +13,13 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import rbasamoyai.createbigcannons.cannons.IncompleteItemCannonBehavior;
 import rbasamoyai.createbigcannons.cannons.ItemCannonBehavior;
 import rbasamoyai.createbigcannons.cannons.autocannon.AutocannonBlockEntity;
 import rbasamoyai.createbigcannons.crafting.WandActionable;
+import rbasamoyai.createbigcannons.crafting.boring.AbstractCannonDrillBlockEntity;
+import rbasamoyai.createbigcannons.crafting.boring.DrillBoringBlockRecipe;
 
 public class IncompleteAutocannonBlockEntity extends AutocannonBlockEntity implements IHaveHoveringInformation, WandActionable {
 
@@ -37,14 +41,22 @@ public class IncompleteAutocannonBlockEntity extends AutocannonBlockEntity imple
 
 	@Override
 	public InteractionResult onWandUsed(UseOnContext context) {
-		if (!(this.getBlockState().getBlock() instanceof IncompleteWithItemsCannonBlock incomplete))
-			return InteractionResult.PASS;
 		if (!this.level.isClientSide) {
+			BlockState state = this.getBlockState();
+			Direction dir = state.getValue(BlockStateProperties.FACING);
+			DrillBoringBlockRecipe recipe = AbstractCannonDrillBlockEntity.getBlockRecipe(state, dir);
 			CompoundTag loadTag = this.saveWithFullMetadata();
+			if (recipe != null) {
+				BlockState newState = recipe.getResultState(state);
+				this.level.setBlock(this.worldPosition, newState, 11);
+			} else if (state.getBlock() instanceof IncompleteWithItemsCannonBlock incomplete) {
+				BlockState newState = incomplete.getCompleteBlockState(state);
+				this.level.setBlock(this.worldPosition, newState, 11);
+			} else {
+				return InteractionResult.PASS;
+			}
 			loadTag.putBoolean("JustBored", true);
-			BlockState boredState = incomplete.getCompleteBlockState(this.getBlockState());
 			this.setRemoved();
-			this.level.setBlock(this.worldPosition, boredState, 11);
 			BlockEntity newBE = this.level.getBlockEntity(this.worldPosition);
 			if (newBE != null) newBE.load(loadTag);
 		}
