4ef2271beb1ecec9cb981d702c914ced0b7b577d
rbasamoyai <b33gy0sh@gmail.com>
2023-12-24 12:06:18 -0500

    🐛 Fixed drop mortar shell not firing, make added_charge_power field explicit in examples, cannon pitch fixes, mount info tooltip formatting

--- common/src/main/java/rbasamoyai/createbigcannons/munitions/big_cannon/BigCannonProjectileProperties.java
+++ common/src/main/java/rbasamoyai/createbigcannons/munitions/big_cannon/BigCannonProjectileProperties.java
@@ -10,7 +10,7 @@ import rbasamoyai.createbigcannons.munitions.config.MunitionPropertiesSerializer
 
 public class BigCannonProjectileProperties extends BaseProjectileProperties {
 
-	private final int addedChargePower;
+	private final float addedChargePower;
 	private final float minimumChargePower;
 	private final boolean canSquib;
 	private final float addedRecoil;
@@ -27,7 +27,7 @@ public class BigCannonProjectileProperties extends BaseProjectileProperties {
 
 	public BigCannonProjectileProperties(String id, JsonObject obj) {
 		super(id, obj);
-		this.addedChargePower = Math.max(0, GsonHelper.getAsInt(obj, "added_charge_power", 0));
+		this.addedChargePower = Math.max(0, GsonHelper.getAsFloat(obj, "added_charge_power", 0));
 		this.minimumChargePower = Math.max(0, GsonHelper.getAsFloat(obj, "minimum_charge_power", 1));
 		this.canSquib = GsonHelper.getAsBoolean(obj, "can_squib", true);
 		this.addedRecoil = Math.max(0, GsonHelper.getAsFloat(obj, "added_recoil", 1));
@@ -35,7 +35,7 @@ public class BigCannonProjectileProperties extends BaseProjectileProperties {
 
 	public BigCannonProjectileProperties(FriendlyByteBuf buf) {
 		super(buf);
-		this.addedChargePower = buf.readVarInt();
+		this.addedChargePower = buf.readFloat();
 		this.minimumChargePower = buf.readFloat();
 		this.canSquib = buf.readBoolean();
 		this.addedRecoil = buf.readFloat();
@@ -44,13 +44,13 @@ public class BigCannonProjectileProperties extends BaseProjectileProperties {
 	@Override
 	public void toNetwork(FriendlyByteBuf buf) {
 		super.toNetwork(buf);
-		buf.writeVarInt(this.addedChargePower)
+		buf.writeFloat(this.addedChargePower)
 			.writeFloat(this.minimumChargePower)
 			.writeBoolean(this.canSquib)
 			.writeFloat(this.addedRecoil);
 	}
 
-	public int addedChargePower() { return this.addedChargePower; }
+	public float addedChargePower() { return this.addedChargePower; }
 	public float minimumChargePower() { return this.minimumChargePower; }
 	public boolean canSquib() { return this.canSquib; }
 	public float addedRecoil() { return this.addedRecoil; }
