b0ad11da269e1eee9edb4de0f0c189392944c7d0
Endalion <34496536+Endalion@users.noreply.github.com>
2024-02-05 15:10:07 +0800

    changed annotation to javax

--- common/src/main/java/rbasamoyai/createbigcannons/mixin/CopycatMixin.java
+++ common/src/main/java/rbasamoyai/createbigcannons/mixin/CopycatMixin.java
@@ -1,21 +1,21 @@
 package rbasamoyai.createbigcannons.mixin;
 
+import javax.annotation.Nonnull;
+
+import org.spongepowered.asm.mixin.Mixin;
+
 import com.simibubi.create.content.decoration.copycat.CopycatBlock;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-
-import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.Mixin;
-
 import rbasamoyai.createbigcannons.munitions.config.PropertiesMimicTypeBlock;
 
 @Mixin(CopycatBlock.class)
 public abstract class CopycatMixin implements PropertiesMimicTypeBlock {
 	@Override
-	public @NotNull Block createBigCannons$getActualBlock(BlockState state, Level level, BlockPos pos) {
+	public @Nonnull Block createBigCannons$getActualBlock(BlockState state, Level level, BlockPos pos) {
 		return CopycatBlock.getMaterial(level, pos).getBlock();
 	}
 }
--- common/src/main/java/rbasamoyai/createbigcannons/munitions/config/PropertiesMimicTypeBlock.java
+++ common/src/main/java/rbasamoyai/createbigcannons/munitions/config/PropertiesMimicTypeBlock.java
@@ -1,12 +1,13 @@
 package rbasamoyai.createbigcannons.munitions.config;
 
+import javax.annotation.Nonnull;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
-import org.jetbrains.annotations.NotNull;
-
 public interface PropertiesMimicTypeBlock {
-	@NotNull Block createBigCannons$getActualBlock(BlockState state, Level level, BlockPos pos);
+	@Nonnull
+	Block createBigCannons$getActualBlock(BlockState state, Level level, BlockPos pos);
 }
