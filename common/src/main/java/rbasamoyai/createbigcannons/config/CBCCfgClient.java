a9664aaa7041457afc21363f9bac951b2d648587
rbasamoyai <b33gy0sh@gmail.com>
2024-05-26 23:30:29 -0400

    improvement, feature: Increased default cannon angle goggles tooltip value precision, added config for it

--- common/src/main/java/rbasamoyai/createbigcannons/config/CBCCfgClient.java
+++ common/src/main/java/rbasamoyai/createbigcannons/config/CBCCfgClient.java
@@ -7,6 +7,7 @@ public class CBCCfgClient extends CBCConfigBase {
 	public final ConfigBool showAutocannonPlumes = b(true, "showAutocannonPlumes");
 	public final ConfigBool showDropMortarPlumes = b(true, "showDropMortarPlumes");
 	public final ConfigInt fluidBlobParticleCount = i(20, 0, 1000, "fluidBlobParticleCount", Comments.fluidBlobParticleCount);
+	public final ConfigInt cannonMountAngleGoggleTooltipPrecision = i(2, 0, 4, "cannonMountAngleGoggleTooltipPrecision", Comments.cannonMountAngleTooltipPrecision);
 
 	public final ConfigGroup screenShake = group(1, "screenShake", "Screen Shake");
 	public final ConfigFloat cannonScreenShakeIntensity = f(1, 0, 2, "cannonScreenShakeIntensity");
@@ -21,6 +22,7 @@ public class CBCCfgClient extends CBCConfigBase {
 	private static class Comments {
 		static String client = "Client-side config for Create Big Cannons.";
 		static String fluidBlobParticleCount = "How many particles are in a Fluid Blob of any size.";
+		static String cannonMountAngleTooltipPrecision = "How many digits are after the angle decimal point on a cannon mount goggle tooltip.";
 	}
 
 }
