4ecb379a93deac925c636eb9d4b5bd88b8fc28d1
rbasamoyai <b33gy0sh@gmail.com>
2026-02-07 16:44:09 -0500

    bug, assets: Fixed cannon drilling - Fixed cannon drill model - Updated cannon drill bit texture - Fixed contraption block syncing to client when drilling

--- src/main/java/rbasamoyai/createbigcannons/network/CBCClientHandlers.java
+++ src/main/java/rbasamoyai/createbigcannons/network/CBCClientHandlers.java
@@ -67,6 +67,7 @@ public class CBCClientHandlers {
                     rbe.loadWithComponents(copy, registryAccess);
 			}
 		}
+        contraption.invalidateClientContraptionStructure();
         contraption.invalidateClientContraptionChildren();
 	}
 
