a964bc2f6c4815be3062bd17227cfc23f3c537a6
rbasamoyai <b33gy0sh@gmail.com>
2026-02-16 01:13:26 -0500

    refactor: Changed ExplosionMixin @Inject to @WrapOperation

--- src/main/java/rbasamoyai/createbigcannons/mixin/ExplosionMixin.java
+++ src/main/java/rbasamoyai/createbigcannons/mixin/ExplosionMixin.java
@@ -1,11 +1,11 @@
 package rbasamoyai.createbigcannons.mixin;
 
+import java.util.Optional;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
@@ -14,7 +14,9 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.ExplosionDamageCalculator;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
@@ -25,12 +27,14 @@ public class ExplosionMixin {
 
 	@Shadow @Final private Level level;
 
-	@Inject(method = "explode",
+	@WrapOperation(method = "explode",
 		at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ExplosionDamageCalculator;getBlockExplosionResistance(Lnet/minecraft/world/level/Explosion;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/material/FluidState;)Ljava/util/Optional;"))
-	private void createbigcannons$explode(CallbackInfo ci, @Local(ordinal = 0) float power, @Local(ordinal = 0) BlockPos blockPos,
-										  @Local(ordinal = 0) BlockState blockState, @Local(ordinal = 0) FluidState fluidState) {
-		if (this instanceof CustomExplosion customExplosion)
-			customExplosion.editBlock(this.level, blockPos, blockState, fluidState, power);
+    private Optional<Float> createbigcannons$explode(ExplosionDamageCalculator instance, Explosion explosion, BlockGetter reader,
+                                                     BlockPos pos, BlockState state, FluidState fluid, Operation<Optional<Float>> original,
+                                                     @Local(ordinal = 0) float power) {
+        if (this instanceof CustomExplosion customExplosion)
+            customExplosion.editBlock(this.level, pos, state, fluid, power);
+        return original.call(instance, explosion, reader, pos, state, fluid);
 	}
 
 	@WrapOperation(method = "finalizeExplosion",
