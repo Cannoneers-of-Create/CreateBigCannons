b2a4a36e6bc0fc63cec71a3fd3a6ef01a9b6208f
rbasamoyai <b33gy0sh@gmail.com>
2026-05-21 10:53:44 -0400

    gameplay: Changed default big cannon and drop mortar recoil scale from 4 to 1

--- src/main/java/rbasamoyai/createbigcannons/config/CBCCfgCannons.java
+++ src/main/java/rbasamoyai/createbigcannons/config/CBCCfgCannons.java
@@ -7,7 +7,7 @@ public class CBCCfgCannons extends ConfigBase {
 	public final ConfigInt maxCannonLength = i(64, 3, "maxCannonLength", Comments.maxCannonLength);
 	public final ConfigInt quickfiringBreechLoadingCooldown = i(40, 0, "quickfiringBreechLoadingCooldown", Comments.quickfiringBreechLoadingCooldown);
 	public final ConfigInt quickfiringBreechOpeningCooldown = i(5, 0, "quickfiringBreechOpeningCooldown", Comments.quickfiringBreechOpeningCooldown);
-	public final ConfigFloat bigCannonRecoilScale = f(4f, 0, "bigCannonRecoilScale");
+	public final ConfigFloat bigCannonRecoilScale = f(1f, 0, "bigCannonRecoilScale");
 	public final ConfigFloat autocannonRecoilScale = f(0.5f, 0, "autocannonRecoilScale");
 
 	public final ConfigGroup dropMortar = group(0, "dropMortar", "Drop Mortar");
