547dc5d85fe9c26523c10fc6b67398f804e8f1d2
rbasamoyai <b33gy0sh@gmail.com>
2026-05-27 20:17:40 -0400

    bug: Reverted mixin update for syncPreciseMotion

--- src/main/java/rbasamoyai/createbigcannons/mixin/client/RPLClientHandlersMixin.java
+++ src/main/java/rbasamoyai/createbigcannons/mixin/client/RPLClientHandlersMixin.java
@@ -1,9 +1,10 @@
 package rbasamoyai.createbigcannons.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.sugar.Local;
 
 import net.minecraft.world.entity.Entity;
@@ -14,9 +15,8 @@ import rbasamoyai.ritchiesprojectilelib.network.RPLClientHandlers;
 @Mixin(RPLClientHandlers.class)
 public class RPLClientHandlersMixin {
 
-	@WrapMethod(method = "syncPreciseMotion", remap = false)
-	private static void createbigcannons$syncPreciseMotion(ClientboundPreciseMotionSyncPacket packet, Operation<Void> original, @Local Entity entity) {
-        original.call(packet);
+	@Inject(method = "syncPreciseMotion", at = @At("TAIL"), remap = false)
+	private static void createbigcannons$syncPreciseMotion(ClientboundPreciseMotionSyncPacket packet, CallbackInfo ci, @Local(name = "entity") Entity entity) {
 		if (entity instanceof AbstractCannonProjectile proj)
 			proj.updateKinematics(packet);
 	}
